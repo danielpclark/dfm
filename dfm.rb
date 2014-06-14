92d9818b0216df8475b974c93d55d21692ab44b8
Daniel P. Clark <6ftdan@gmail.com>
2014-06-14 02:12:19 -0400

    First

---
+++ dfm.rb
@@ -0,0 +1,42 @@
+#!/user/bin/ruby
+require 'digest'
+require 'json'
+
+class DFM
+	def initialize
+		@file_list = {}
+		@hashFunc = Digest::MD5.new
+	end
+
+	def recurse_current
+		Dir.glob('**' + File::SEPARATOR + '*').each { |file|
+			if File.file? file
+				insert_file file
+			end
+		}
+	end
+
+	private
+
+	def insert_file(file)
+		file_io = File.open(file, "rb") { |io| io.read }
+		hex_file = @hashFunc.hexdigest(file_io)
+		@file_list[ hex_file ] = Array( @file_list[ hex_file ] ) << file
+	end
+
+	def select_duplicates
+		@file_list.select {|k,v| v.length > 2 }
+	end
+
+	def print_json(list)
+		puts list.to_json
+	end
+
+	def print_duplicates
+		print_json select_duplicates
+	end
+end
+
+program = DFM.new
+program.recurse_current
+program.print_duplicates
