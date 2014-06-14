61ce856659caf41bc787b8a7492e7a8456f3a5ef
Daniel P. Clark <6ftdan@gmail.com>
2014-06-14 02:18:39 -0400

    .

--- dfm.rb
+++ dfm.rb
@@ -9,7 +9,7 @@ class DFM
 	end
 
 	def recurse_current
-		Dir.glob('**' + File::SEPARATOR + '*').each { |file|
+		Dir.glob( '**' + File::SEPARATOR + '*' ).each { |file|
 			if File.file? file
 				insert_file file
 			end
@@ -18,17 +18,17 @@ class DFM
 
 	private
 
-	def insert_file(file)
+	def insert_file( file )
 		file_io = File.open(file, "rb") { |io| io.read }
-		hex_file = @hashFunc.hexdigest(file_io)
+		hex_file = @hashFunc.hexdigest( file_io )
 		@file_list[ hex_file ] = Array( @file_list[ hex_file ] ) << file
 	end
 
 	def select_duplicates
-		@file_list.select {|k,v| v.length > 2 }
+		@file_list.select { |k,v| v.length > 2 }
 	end
 
-	def print_json(list)
+	def print_json( list )
 		puts list.to_json
 	end
 
