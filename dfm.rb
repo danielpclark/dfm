8907586c373549882907148676994e88a5751937
Daniel P. Clark <6ftdan@gmail.com>
2014-06-15 13:36:32 -0400

    Added file name duplicates option.

--- dfm.rb
+++ dfm.rb
@@ -4,39 +4,48 @@ require 'json'
 
 class DFM
 	def initialize
-		@file_list = {}
+		@files_by_hexdigest = {}
+		@files_by_name = {}
 		@hashFunc = Digest::MD5.new
 	end
 
-	def recurse_current
-		Dir.glob( '**' + File::SEPARATOR + '*' ).each { |file|
+	def recurse_path( path = '.'::+(File::SEPARATOR) )
+		Dir.glob( path + '**' + File::SEPARATOR + '*' ).each { |file|
 			if File.file? file
 				insert_file file
 			end
 		}
 	end
 
-	def print_duplicates
-		print_json select_duplicates
+	def print_duplicates( opt = "hex" )
+		if !!opt["hex"]
+			print_json select_duplicates
+		elsif !!opt["name"]
+			print_json select_duplicates(@files_by_name)
+		end
 	end
 
 	private
 
 	def insert_file( file )
+		# hex
 		file_io = File.open(file, "rb") { |io| io.read }
 		hex_file = @hashFunc.hexdigest( file_io )
-		@file_list[ hex_file ] = Array( @file_list[ hex_file ] ) << file
+		@files_by_hexdigest[ hex_file ] = Array( @files_by_hexdigest[ hex_file ] ) << file
+		# name
+		@files_by_name[ File.basename(file) ] = Array( @files_by_name[ File.basename(file) ] ) << file
 	end
 
-	def select_duplicates
-		@file_list.select { |k,v| v.length > 2 }
+	def select_duplicates( hash = @files_by_hexdigest )
+		hash.select { |k,v| v.length > 2 }
 	end
 
-	def print_json( list )
-		puts JSON.pretty_generate(list)
+	def print_json( hash )
+		puts JSON.pretty_generate( hash )
 	end
 end
 
 program = DFM.new
-program.recurse_current
+program.recurse_path
 program.print_duplicates
+program.print_duplicates("name")
