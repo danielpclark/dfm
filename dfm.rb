8e5803e3ab3cae61afa89978d9fa08909d8804f6
Daniel P. Clark <6ftdan@gmail.com>
2014-06-15 15:51:22 -0400

    Commandline options added.

--- dfm.rb
+++ dfm.rb
@@ -8,11 +8,12 @@ class DFM
 	def initialize( params = {} )
 		@files_by_hexdigest = {}
 		@files_by_name = {}
-		@filters = Array( params.fetch(:filters, nil) )
+		@filters = Array( params.fetch( :filters, nil ) )
+		@path = ( ( params.fetch( :path, '.' ) ) || '.' ) + File::SEPARATOR
 		@hashFunc = Digest::MD5.new
 	end
 
-	def recurse_path( path = '.'::+( File::SEPARATOR ) )
+	def recurse_path( path = @path )
 		@filters.empty? ? ( filters = "" ) : ( filters = @filters.join( "," ).prepend( ".{" ).<<( "}" ) )
 		Dir.glob( path + '**' + File::SEPARATOR + '*' + filters ).each { |file|
 			if File.file? file
@@ -22,10 +23,10 @@ class DFM
 	end
 
 	def print_duplicates( opt = "hex" )
-		if !!opt["hex"]
-			print_json select_duplicates
-		elsif !!opt["name"]
-			print_json select_duplicates(@files_by_name)
+		if !!opt[ "hex" ]
+			print_json hex
+		elsif !!opt[ "name" ]
+			print_json name
 		end
 	end
 
@@ -34,18 +35,18 @@ class DFM
 	end
 
 	def name
-		select_duplicates(@files_by_name)
+		select_duplicates( @files_by_name )
 	end
 
 	private
 
 	def insert_file( file )
 		# hex
-		file_io = File.open(file, "rb") { |io| io.read }
+		file_io = File.open( file, "rb" ) { |io| io.read }
 		hex_file = @hashFunc.hexdigest( file_io )
 		@files_by_hexdigest[ hex_file ] = Array( @files_by_hexdigest[ hex_file ] ) << file
 		# name
-		@files_by_name[ File.basename(file) ] = Array( @files_by_name[ File.basename(file) ] ) << file
+		@files_by_name[ File.basename( file ) ] = Array( @files_by_name[ File.basename( file ) ] ) << file
 	end
 
 	def select_duplicates( hash = @files_by_hexdigest )
@@ -62,8 +63,17 @@ end
 # program = DFM.new filters: "jpg"
 # program = DFM.new filters: ["jpg","gif","png"]
 if __FILE__ == $0
-	program = DFM.new
+	require 'optparse'
+	options = {}
+	OptionParser.new do |opts|
+		opts.banner = "Usage: dfm [options] [path]"
+		opts.on("-f", "--filters FILTERS", Array, "File extension filters") do |filters|
+			options[:filters] = filters
+		end
+	end.parse!
+	path = ARGV.select {|i| File.directory? i }[0]
+	program = DFM.new( options.update( path: path ) )
 	program.recurse_path
 	program.print_duplicates
-	program.print_duplicates("name")
+	program.print_duplicates( "name" )
 end
