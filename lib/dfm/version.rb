0cc4068e0fa506a788745d8e843a34f1c1800e2a
Daniel P. Clark <6ftdan@gmail.com>
2017-01-25 14:58:07 -0500

    typo fix
    [ci skip]

--- lib/dfm/version.rb
+++ lib/dfm/version.rb
@@ -8,7 +8,7 @@ end
 # Version 0.0.4
 # * RDOC
 # Version 0.0.3
-# * Updated commandline app to merge miltiple JSON searches as single JSON output
+# * Updated commandline app to merge multiple JSON searches as single JSON output
 # Version 0.0.2
 # * A little more DRY
 # * Fixed duplicates of only 2
